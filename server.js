7df336b48824c90d68aeef0977847a82e06a3a84
Kwok yan Lau <dorislau@kwoks-mbp.lan>
2022-09-02 07:05:51 -0700

    second

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //////////////////////////////////// REQUIRE DEPENDENCIES ////////////////////////////////////
 const express = require('express');
 const pokemons = require('./models/pokemon.js');
-// const methodOverride = require("method-override");
+const methodOverride = require("method-override");
 
 
 //////////////////////////////////// INITIALIZE EXPRESS APP ////////////////////////////////////
@@ -17,8 +17,8 @@ const port = 3000;
 
 // This adds data to req.body so we can access it in the CREATE action 
 
-// app.use(express.urlencoded({ extended: false }));
-// app.use(methodOverride("_method"))
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"))
 
 //////////////////////////////////// DEFINE OUR ROUTES  ////////////////////////////////////
 // INDUCES (FOLLOW THE FORMAT) //
@@ -44,31 +44,21 @@ app.get("/pokemons/new", (req, res)=>{
 
 //////////////////////////////////// UPDATE ////////////////////////////////////
 // JUST THIS WILL ONLY ADD THE EDIT ITEM TO THE LIST //
-// app.get("/fruits/:indexOfFruitsArray/edit", (req, res) =>{
-//   res.render(
-//     "edit.ejs",
-//     {
-//       fruit: fruits[req.params.indexOfFruitsArray],
-//       index: req.params.indexOfFruitsArray
-//     }
-//   )
-// });
-
-
-
-// WE NEED THIS TO MAKE SURE THE EDIT IS EDITING NOT ADDING  //
-// app.put("/fruits/:indexOfFruitsArray", (req, res) => {
-//   //:indexOfFruitsArray is the index of our fruits array that we want to change
-//   if (req.body.readyToEat === "on") {
-//     //if checked, req.body.readyToEat is set to 'on'
-//     req.body.readyToEat = true
-//   } else {
-//     //if not checked, req.body.readyToEat is undefined
-//     req.body.readyToEat = false
-//   }
-//   fruits[req.params.indexOfFruitsArray] = req.body //in our fruits array, find the index that is specified in the url (:indexOfFruitsArray).  Set that element to the value of req.body (the input data)
-//   res.redirect("/fruits") //redirect to the index page
-// })
+app.get("/pokemons/:indexOfPokemonArray/edit", (req, res) =>{
+    res.render(
+      "pokemon_edit.ejs",
+      {
+        pokemon: pokemons[req.params.indexOfPokemonArray],
+        index: req.params.indexOfPokemonArray
+      }
+    )
+  });
+  
+  app.put("/pokemons/:indexOfPokemonArray", (req, res) => {
+    //:indexOfFruitsArray is the index of our fruits array that we want to change
+    pokemons[req.params.indexOfPokemonArray] = req.body //in our fruits array, find the index that is specified in the url (:indexOfFruitsArray).  Set that element to the value of req.body (the input data)
+    res.redirect("/pokemons/") //redirect to the index page
+  })
 
 
 //////////////////////////////////// CREATE ////////////////////////////////////
@@ -96,10 +86,147 @@ app.get("/pokemons/new", (req, res)=>{
 
 //////////////////////////////////// SHOW ////////////////////////////////////
 app.get("/pokemons/:indexOfPokemonArray", (req, res)=>{  
-      res.render("pokemon_show.ejs", {
-        pokemon: pokemons[req.params.indexOfPokemonArray]
-    })
+        const pokemon = pokemons[req.params.indexOfPokemonArray]
+        
+        if (pokemon.type.length === 1) {
+            res.send(`<button><a href="/pokemons">Return to Pokedex</a></button>
+            <button><a href="/pokemons/:indexOfPokemonArray/edit">Edit Pokemon Information</a></button>
+            <div class="pokemonData1"><img src="${pokemon.img}" width="200" height="200">
+            <h2>id# ${pokemon.id}</h2>
+            <h1>${pokemon.name}</h1>
+            <ul>
+            <li>${pokemon.type[0]}</li>
+            </ul>
+            </div>
+            <div class="pokemonStates">
+            <h2>Stats</h2>
+            <ul>
+            <li>HP: ${pokemon.stats.hp}</li>
+            <li>Attack: ${pokemon.stats.attack}</li>
+            <li>Defense: ${pokemon.stats.defense}</li>
+            <li>Spattack: ${pokemon.stats.spattack}</li>
+            <li>Spdefense: ${pokemon.stats.spdefense}</li>
+            <li>Speed: ${pokemon.stats.speed}</li>
+            </ul>
+            </div>
+            <<div class="pokemonDemages">
+            <h2>Damages</h2>
+            <ul>
+            <li>Normal: ${pokemon.damages.normal}</li>
+            <li>Fire: ${pokemon.damages.fire}</li>
+            <li>Water: ${pokemon.damages.water}</li>
+            <li>Electric: ${pokemon.damages.electric}</li>
+            <li>Grass: ${pokemon.damages.grass}</li>
+            <li>Ice: ${pokemon.damages.ice}</li>
+            <li>Fight: ${pokemon.damages.fight}</li>
+            <li>Poison: ${pokemon.damages.poison}</li>
+            <li>Ground: ${pokemon.damages.ground}</li>
+            <li>Flying: ${pokemon.damages.flying}</li>
+            <li>psychic: ${pokemon.damages.psychic}</li>
+            <li>Bug: ${pokemon.damages.bug}</li>
+            <li>Rock: ${pokemon.damages.rock}</li>
+            <li>Ghost: ${pokemon.damages.ghost}</li>
+            <li>Dragon: ${pokemon.damages.dragon}</li>
+            <li>Dark: ${pokemon.damages.dark}</li>
+            <li>Steel: ${pokemon.damages.steel}</li>
+            </ul>
+            </div>
+            <button><a href="/pokemons">Return to Pokedex</a></button>`)
+        } else if (pokemon.type.length === 2) {
+            res.send(`<button><a href="/pokemons">Return to Pokedex</a></button>
+            <button><a href="/pokemons/:indexOfPokemonArray/edit">Edit Pokemon Information</a></button>
+            <div class="pokemonData1"><img src = "${pokemon.img}" width="200" height="200">
+            <h2>id# ${pokemon.id}</h2>
+            <h1>${pokemon.name}</h1>
+            <ul>
+            <li>${pokemon.type[0]}</li>
+            <li>${pokemon.type[1]}</li>
+            </ul>
+            </div>
+            <div class="pokemonStates">
+            <h2>Stats</h2>
+            <ul>
+            <li>HP: ${pokemon.stats.hp}</li>
+            <li>Attack: ${pokemon.stats.attack}</li>
+            <li>Defense: ${pokemon.stats.defense}</li>
+            <li>Spattack: ${pokemon.stats.spattack}</li>
+            <li>Spdefense: ${pokemon.stats.spdefense}</li>
+            <li>Speed: ${pokemon.stats.speed}</li>
+            </ul>
+            </div>
+            <<div class="pokemonDemages">
+            <h2>Damages</h2>
+            <ul>
+            <li>Normal: ${pokemon.damages.normal}</li>
+            <li>Fire: ${pokemon.damages.fire}</li>
+            <li>Water: ${pokemon.damages.water}</li>
+            <li>Electric: ${pokemon.damages.electric}</li>
+            <li>Grass: ${pokemon.damages.grass}</li>
+            <li>Ice: ${pokemon.damages.ice}</li>
+            <li>Fight: ${pokemon.damages.fight}</li>
+            <li>Poison: ${pokemon.damages.poison}</li>
+            <li>Ground: ${pokemon.damages.ground}</li>
+            <li>Flying: ${pokemon.damages.flying}</li>
+            <li>psychic: ${pokemon.damages.psychic}</li>
+            <li>Bug: ${pokemon.damages.bug}</li>
+            <li>Rock: ${pokemon.damages.rock}</li>
+            <li>Ghost: ${pokemon.damages.ghost}</li>
+            <li>Dragon: ${pokemon.damages.dragon}</li>
+            <li>Dark: ${pokemon.damages.dark}</li>
+            <li>Steel: ${pokemon.damages.steel}</li>
+            </ul>
+            </div>
+            <button><a href="/pokemons">Return to Pokedex</a></button>`)
+        } else if (pokemon.type.length === 2) {
+            res.send(`<button><a href="/pokemons">Return to Pokedex</a></button>
+            <button><a href="/pokemons/:indexOfPokemonArray/edit">Edit Pokemon Information</a></button>
+            <div class="pokemonData1"><img src = "${pokemon.img}" width="200" height="200">
+            <h2>id# ${pokemon.id}</h2>
+            <h1>${pokemon.name}</h1>
+            <ul>
+            <li>${pokemon.type[0]}</li>
+            <li>${pokemon.type[1]}</li>
+            <li>${pokemon.type[2]}</li>
+            </ul>
+            </div>
+            <div class="pokemonStates">
+            <h2>Stats</h2>
+            <ul>
+            <li>HP: ${pokemon.stats.hp}</li>
+            <li>Attack: ${pokemon.stats.attack}</li>
+            <li>Defense: ${pokemon.stats.defense}</li>
+            <li>Spattack: ${pokemon.stats.spattack}</li>
+            <li>Spdefense: ${pokemon.stats.spdefense}</li>
+            <li>Speed: ${pokemon.stats.speed}</li>
+            </ul>
+            </div>
+            <<div class="pokemonDemages">
+            <h2>Damages</h2>
+            <ul>
+            <li>Normal: ${pokemon.damages.normal}</li>
+            <li>Fire: ${pokemon.damages.fire}</li>
+            <li>Water: ${pokemon.damages.water}</li>
+            <li>Electric: ${pokemon.damages.electric}</li>
+            <li>Grass: ${pokemon.damages.grass}</li>
+            <li>Ice: ${pokemon.damages.ice}</li>
+            <li>Fight: ${pokemon.damages.fight}</li>
+            <li>Poison: ${pokemon.damages.poison}</li>
+            <li>Ground: ${pokemon.damages.ground}</li>
+            <li>Flying: ${pokemon.damages.flying}</li>
+            <li>psychic: ${pokemon.damages.psychic}</li>
+            <li>Bug: ${pokemon.damages.bug}</li>
+            <li>Rock: ${pokemon.damages.rock}</li>
+            <li>Ghost: ${pokemon.damages.ghost}</li>
+            <li>Dragon: ${pokemon.damages.dragon}</li>
+            <li>Dark: ${pokemon.damages.dark}</li>
+            <li>Steel: ${pokemon.damages.steel}</li>
+            </ul>
+            </div>
+            <button><a href="/pokemons">Return to Pokedex</a></button>`)
+    };
+
 });
+
 
 // TELL OUR APP TO LISTEN ON PORT...
 app.listen(port, ()=>{
