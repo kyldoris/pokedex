bf223ae767f67178edb542586e807b01b59cf32d
Kwok yan Lau <dorislau@kwoks-mbp.lan>
2022-09-19 19:14:14 -0700

    delete and add

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const pokemons = require('./models/pokemon.js');
 const methodOverride = require("method-override");
+const bodyParser = require("body-parser")
 
 
 //////////////////////////////////// INITIALIZE EXPRESS APP ////////////////////////////////////
@@ -16,15 +17,16 @@ const port = 3000;
 // })
 
 // This adds data to req.body so we can access it in the CREATE action 
-
-app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"))
+
 
 //////////////////////////////////// DEFINE OUR ROUTES  ////////////////////////////////////
 // INDUCES (FOLLOW THE FORMAT) //
 
 //////////////////////////////////// INDEX ////////////////////////////////////
-app.get("/pokemons/", (req, res)=>{
+app.get("/pokemons", (req, res)=>{
     res.render("pokemon_index.ejs", {
       allPokemons: pokemons
     })
@@ -36,61 +38,50 @@ app.get("/pokemons/new", (req, res)=>{
   res.render("pokemon_new.ejs")
 });
 
-//////////////////////////////////// DELETE ////////////////////////////////////
-// app.delete("/fruits/:indexOfFruitsArray", (req, res) =>{
-//   fruits.splice(req.params.indexOfFruitsArray, 1)
-//   res.redirect("/fruits")
-// });
+////////////////////////////////// DELETE ////////////////////////////////////
+app.delete("/pokemons/:id", (req, res) =>{
+  pokemons.splice(req.params.id, 1)
+  res.redirect("/pokemons")
+});
 
 //////////////////////////////////// UPDATE ////////////////////////////////////
 // JUST THIS WILL ONLY ADD THE EDIT ITEM TO THE LIST //
-app.get("/pokemons/:indexOfPokemonArray/edit", (req, res) =>{
+
+  
+  app.put("/pokemons/:id", (req, res) => {
+    //:indexOfFruitsArray is the index of our fruits array that we want to change
+    pokemon[req.params.id] = req.body //in our fruits array, find the index that is specified in the url (:indexOfFruitsArray).  Set that element to the value of req.body (the input data)
+    res.redirect("/pokemons") //redirect to the index page
+  })
+
+  app.get("/pokemons/:id/edit", (req, res) =>{
     res.render(
       "pokemon_edit.ejs",
       {
-        pokemon: pokemons[req.params.indexOfPokemonArray],
-        index: req.params.indexOfPokemonArray
+        pokemon: pokemons[req.params.id],
+        id: req.params.id
       }
     )
   });
-  
-  app.put("/pokemons/:indexOfPokemonArray", (req, res) => {
-    //:indexOfFruitsArray is the index of our fruits array that we want to change
-    pokemons[req.params.indexOfPokemonArray] = req.body //in our fruits array, find the index that is specified in the url (:indexOfFruitsArray).  Set that element to the value of req.body (the input data)
-    res.redirect("/pokemons/") //redirect to the index page
-  })
 
 
 //////////////////////////////////// CREATE ////////////////////////////////////
-// app.post("/fruits", (req, res)=>{
-//   console.log(req.body);
-//   if (req.body.readyToEat === "on") {
-//     //if checked, req.body.readyToEat is set to 'on'
-//     req.body.readyToEat = true //do some data correction
-//   } else {
-//     //if not checked, req.body.readyToEat is undefined
-//     req.body.readyToEat = false //do some data correction
-//   }
-//   fruits.push(req.body)
-//   res.redirect("/fruits")
-// });
+app.post("/pokemons", (req, res)=>{
+  pokemons.push(req.body)
+  res.redirect("/pokemons")
+});
 
-//////////////////////////////////// TESTING ROUTE ////////////////////////////////////
-// app.post("/pokemons", (req, res) => {
-//   console.log("Create route accessed!")
-//   console.log("Req.body is: ", req.body)
-//   res.send(req.body)
-// })
 
 // E
 
+
 //////////////////////////////////// SHOW ////////////////////////////////////
-app.get("/pokemons/:indexOfPokemonArray", (req, res)=>{  
-        const pokemon = pokemons[req.params.indexOfPokemonArray]
+app.get("/pokemons/:id", (req, res)=>{  
+        const pokemon = pokemons[req.params.id]
         
         if (pokemon.type.length === 1) {
             res.send(`<button><a href="/pokemons">Return to Pokedex</a></button>
-            <button><a href="/pokemons/:indexOfPokemonArray/edit">Edit Pokemon Information</a></button>
+            <button><a href="/pokemons/:id/edit">Edit Pokemon Information</a></button>
             <div class="pokemonData1"><img src="${pokemon.img}" width="200" height="200">
             <h2>id# ${pokemon.id}</h2>
             <h1>${pokemon.name}</h1>
@@ -109,7 +100,7 @@ app.get("/pokemons/:indexOfPokemonArray", (req, res)=>{
             <li>Speed: ${pokemon.stats.speed}</li>
             </ul>
             </div>
-            <<div class="pokemonDemages">
+            <div class="pokemonDemages">
             <h2>Damages</h2>
             <ul>
             <li>Normal: ${pokemon.damages.normal}</li>
@@ -134,7 +125,7 @@ app.get("/pokemons/:indexOfPokemonArray", (req, res)=>{
             <button><a href="/pokemons">Return to Pokedex</a></button>`)
         } else if (pokemon.type.length === 2) {
             res.send(`<button><a href="/pokemons">Return to Pokedex</a></button>
-            <button><a href="/pokemons/:indexOfPokemonArray/edit">Edit Pokemon Information</a></button>
+            <button><a href="/pokemons/:id/edit">Edit Pokemon Information</a></button>
             <div class="pokemonData1"><img src = "${pokemon.img}" width="200" height="200">
             <h2>id# ${pokemon.id}</h2>
             <h1>${pokemon.name}</h1>
@@ -177,9 +168,9 @@ app.get("/pokemons/:indexOfPokemonArray", (req, res)=>{
             </ul>
             </div>
             <button><a href="/pokemons">Return to Pokedex</a></button>`)
-        } else if (pokemon.type.length === 2) {
+        } else if (pokemon.type.length === 3) {
             res.send(`<button><a href="/pokemons">Return to Pokedex</a></button>
-            <button><a href="/pokemons/:indexOfPokemonArray/edit">Edit Pokemon Information</a></button>
+            <button><a href="/pokemons/:id/edit">Edit Pokemon Information</a></button>
             <div class="pokemonData1"><img src = "${pokemon.img}" width="200" height="200">
             <h2>id# ${pokemon.id}</h2>
             <h1>${pokemon.name}</h1>
@@ -231,4 +222,4 @@ app.get("/pokemons/:indexOfPokemonArray", (req, res)=>{
 // TELL OUR APP TO LISTEN ON PORT...
 app.listen(port, ()=>{
     console.log(`listening on port `, port)
-});
+  })
